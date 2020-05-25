303b749cb4a5a77490d54e631266c303326c2a83
Ryan Paixao <Ryan.paixao@endurance.com>
2020-05-25 00:05:04 -0400

    Add project routes.

--- server.js
+++ server.js
@@ -16,23 +16,32 @@ app.use(bodyParser.json())
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// database
+const db = require('./app/models')
+const Role = db.role
+
+// Use in Production
+// db.sequelize.sync()
+
+// Use in Development
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and Resync Db')
+  initial()
+})
+
 // simple route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Paper Telephone app!'})
 })
 
+// routes
+require('./app/routes/auth.routes')(app)
+require('./app/routes/user.routes')(app)
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!!!`)
-})
-
-const db = require('./app/models')
-const Role = db.role
-
-db.sequelize.sync({ force: true }).then(() => {
-  console.log('Drop and Resync Db')
-  initial()
 })
 
 function initial() {
